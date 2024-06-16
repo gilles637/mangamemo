b67d25dbd2dd82b049e04108970618eb4aaaa1f8
gilles637 <tillaym02@gmail.com>
2024-06-16 21:58:34 +0200

    Premier commit

---
+++ src/main/java/dev/gilles/mangamemo/MangamemoApplication.java
@@ -0,0 +1,13 @@
+package dev.gilles.mangamemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MangamemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MangamemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/gilles/mangamemo/MangamemoApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.gilles.mangamemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MangamemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
